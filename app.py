a58977e40dc43f4f6512d5d75f7b4f2b6f22da76
Sanja Rishi <rishisanja99@gmail.com>
2026-04-09 17:49:23 +0530

    //

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ load_dotenv()
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
 # ── Secure Email config ──────────────────────────────────────────────
-app.config['MAIL_SERVER']   = 'smtp.gmail.com'
+app.config['MAIL_SERVER']   = os.getenv('MAIL_SERVER','smtp.gmail.com')
 app.config['MAIL_PORT']     = 587
 app.config['MAIL_USE_TLS']  = True
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
